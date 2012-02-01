dd547c64a2b35d3c8b9b34f71798a07dd426255b
Mildred Ki'Lya <silkensedai@online.fr>
2012-02-01 17:20:38 +0100

    wip

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
+import os
 
-admin_login = 'admin'
-admin_pass  = None
 debug       = False
 configdir   = '/etc/courier'
 userdir     = '/srv/courier'
 virt_user   = 'vmail'
 virt_group  = 'vmail'
+admin_login = 'admin'
+admin_pass  = None
+
+os.environ['COURIER_CONFIG_DIR'] = configdir
+
 
