b4b5fbc8692f5405519f2e9e1d64702b2ac0e464
Mildred Ki'Lya <silkensedai@online.fr>
2012-02-01 17:53:13 +0100

    wip

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ virt_user   = 'vmail'
 virt_group  = 'vmail'
 admin_login = 'admin'
 admin_pass  = None
+helper_path = os.path.abspath(ospath.join(os.path.dirname(os.path.abspath(__file__)), "..", "bin", "courier-config"))
 
 os.environ['COURIER_CONFIG_DIR'] = configdir
 
--- src/system.py
+++ src/system.py
@@ -1,4 +1,5 @@
 import subprocess
+import config
 
 def gid_for_group(group):
   with open("/etc/group") as f:
@@ -14,12 +15,15 @@ def uid_for_user(user):
       if elems[0] == user:
         return int(elems[2])
 
-def call(args):
+def call(args, input=None):
   log = "> " + " ".join(args) + "\n"
   p = subprocess.Popen(args, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  out, _ = p.communicate()
+  out, _ = p.communicate(input=input)
   log += out
   if p.returncode != 0:
     raise Exception("Returned %d:\n> %s\n%s" % (p.returncode, " ".join(args), out))  
   return log
+
+def call_helper(args, input=None):
+  return call([config.helper_path] + args, input=input)
 
